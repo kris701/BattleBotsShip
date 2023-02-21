9857f5f1e43c96884d6d1c1856f6162ceac42d61
Kristian Skov <kris701kj@gmail.com>
2023-02-21 11:43:10 +0100

    Updated a visual ship designer model

--- BattleBotsShip/Views/UserControls/VisualShipModel.xaml.cs
+++ BattleBotsShip/Views/UserControls/VisualShipModel.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,17 @@ namespace BattleBotsShip.Views.UserControls
         public VisualShipModel()
         {
             InitializeComponent();
+        }
+
+        private void NumbersOnly_TextChanged(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void UpdateField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
